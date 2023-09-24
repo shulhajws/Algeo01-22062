09c9b84ceb7e8b33cafe454e537e003280a25898
hiirr <zahiradina2303@gmail.com>
2023-09-24 15:22:49 +0700

    1st gauss

--- src/mylib/AljabarLinear.java
+++ src/mylib/AljabarLinear.java
@@ -34,4 +34,23 @@ public class AljabarLinear {
             return det;
         }
     }
+
+    public static void Gauss(Matriks m){
+        int r = 0; int c = 0;
+        while (!gauss && c < m.nCols){
+            if (m.Matriks[r][c] != 1){
+                double x = m.Matriks[r][c];
+                convertOne(x, r, m); 
+            } else {
+                continue;
+            }
+            for(int i = r + 1; r < m.nRows; r++){
+                double x = m.Matriks[r][c];
+                int rx = findFirstOne(m, c);
+                convertZero(x, rx, i);
+            }
+            r = findFirstOne(m, c);
+            c++; r++;1`1121
+        }
+    }
 }
---
+++ src/mylib/Gauss.java
@@ -0,0 +1,41 @@
+package mylib;
+
+// import Matriks.*;
+
+public class Gauss {
+    public void convertOne(double x, int r, Matriks m){
+        for(int c = 0; c < m.nCols; c++){
+            m.Matriks[r][c] = m.Matriks[r][c] / x;
+        }
+    }
+
+    public void convertZero(double x, int rx, int ry, Matriks m){
+        for(int c = 0; c < m.nCols; c++){
+            m.Matriks[ry][c] = m.Matriks[ry][c] - (x * m.Matriks[rx][c]);
+        }
+    }
+
+    public int findFirstOne(Matriks m, int c){
+        int row = 0;
+        for(int r = 0; r < m.nRows; r++){
+            if (m.Matriks[r][c] == 1){
+                row = r;
+                break;
+            }
+        }
+        return row;
+    }
+
+    public boolean isRowAllZero(Matriks m, int r){
+        int count = 0; boolean yes = false;
+        for(int c = 0; c < m.nCols; c++){
+            if (m.Matriks[r][c] == 0){
+                count++;
+            }
+        }
+        if (count == m.nCols){
+            yes = true;
+        }
+        return yes;
+    }
+}
